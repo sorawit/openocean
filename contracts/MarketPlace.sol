46cd16039800927889a7477810322a3a571f908a
Sorawit Suriyakarn <thepsint@gmail.com>
2021-08-25 12:20:25 +0000

    Add operator signature

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/access/Ownable.sol';
+import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/access/AccessControl.sol';
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/token/ERC20/IERC20.sol';
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/token/ERC20/utils/SafeERC20.sol';
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/token/ERC721/IERC721.sol';
@@ -15,42 +15,56 @@ struct Order {
   address nft; // The NFT contract address to be traded
   uint id; // The NFT id to be traded
   bool isBuy; // Whether the maker wants to buy or sell the NFT
-  uint cost; // How many denom tokens does it cost to perform trade
-  address denom; // The ERC20 contract address for denominator
-  uint64 expr; // Expiration timestamp in UNIX epoch
+  uint cost; // How many unit tokens does it cost to perform trade
+  address unit; // The ERC20 contract address for cost unit
+  uint64 expiration; // Expiration timestamp in UNIX epoch
   uint64 salt; // Unique salt
 }
 
-/// @dev Decentralized market place for trading NFTs without fees. Although the contract is
-/// ownable, the only action that the owner can do is to stop trading activity. CANNOT STEAL FUNDS.
-contract MarketPlace is Ownable, Pausable, ReentrancyGuard {
+/// @dev Decentralized market place for trading NFTs without fees. Although the contract has
+/// access control, the most the owner can do is stopping trading activity. CANNOT STEAL FUNDS.
+contract MarketPlace is AccessControl, Pausable, ReentrancyGuard {
   using SafeERC20 for IERC20;
+
+  bytes32 public OPERATOR_ROLE = keccak256('OPERATOR_ROLE');
   mapping(bytes32 => bool) public gone;
 
+  constructor() {
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
   /// @dev Emergency call by the governor to stop trading activity. CANNOT STEAL FUNDS.
-  function pause() external onlyOwner {
+  function pause() external onlyRole(DEFAULT_ADMIN_ROLE) {
     _pause();
   }
 
   /// @dev Unpause this contract and allow trading activity to continue.
-  function unpause() external onlyOwner {
+  function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) {
     _unpause();
   }
 
   /// @dev Perform a trade, using ord.maker+sig as maker and msg.sender as taker.
-  function trade(Order memory ord, bytes memory sig) external whenNotPaused nonReentrant {
-    require(ord.expr > block.timestamp, '!expr');
+  function trade(
+    Order memory ord,
+    bytes memory msig,
+    uint64 deadline,
+    bytes memory osig
+  ) external whenNotPaused nonReentrant {
+    require(deadline > block.timestamp, '!deadline');
+    require(ord.expiration > block.timestamp, '!expiration');
     require(ord.maker != msg.sender && ord.maker != address(this), '!maker');
-    require(ord.nft != msg.sender && ord.nft != address(this), '!nft');
-    require(ord.denom != msg.sender && ord.denom != address(this), '!denom');
+    require(ord.nft != msg.sender && ord.nft != address(this) && ord.nft != ord.maker, '!nft');
+    require(ord.unit != msg.sender && ord.unit != address(this) && ord.unit != ord.maker, '!unit');
     bytes32 hash = getOrderHash(ord);
     require(!gone[hash], '!gone');
     gone[hash] = true;
-    bytes32 sign = ECDSA.toEthSignedMessageHash(hash);
-    require(SignatureChecker.isValidSignatureNow(ord.maker, sign, sig), '!sig');
+    bytes32 msign = ECDSA.toEthSignedMessageHash(hash);
+    require(SignatureChecker.isValidSignatureNow(ord.maker, msign, msig), '!msig');
+    bytes32 osign = ECDSA.toEthSignedMessageHash(keccak256(abi.encodePacked(hash, deadline)));
+    require(hasRole(OPERATOR_ROLE, ECDSA.recover(osign, osig)), '!osig');
     address buyer = ord.isBuy ? ord.maker : msg.sender;
     address seller = ord.isBuy ? msg.sender : ord.maker;
-    IERC20(ord.denom).safeTransferFrom(buyer, seller, ord.cost);
+    IERC20(ord.unit).safeTransferFrom(buyer, seller, ord.cost);
     IERC721(ord.nft).safeTransferFrom(seller, buyer, ord.id, '');
   }
 
@@ -66,8 +80,8 @@ contract MarketPlace is Ownable, Pausable, ReentrancyGuard {
   function getOrderHash(Order memory ord) public view returns (bytes32) {
     // prettier-ignore
     return keccak256(abi.encodePacked(
-      address(this), ord.maker, ord.nft, ord.id, ord.isBuy,
-      ord.cost, ord.denom, ord.expr, ord.salt
+      block.chainid, address(this), ord.maker, ord.nft, ord.id, ord.isBuy,
+      ord.cost, ord.unit, ord.expiration, ord.salt
     ));
   }
 }
--- tests/test_market.py
+++ tests/test_market.py
@@ -6,10 +6,14 @@ from brownie import a, web3, MockNFT, MockERC20, MarketPlace
 def order_hash(market, order):
     return web3.solidityKeccak(
         [
-            'address', 'address', 'address', 'uint256', 'bool', 'uint256', 'address', 'uint64', 'uint64'
+            'uint256', 'address', 'address', 'address', 'uint256', 'bool', 'uint256', 'address', 'uint64', 'uint64'
         ],
-        [market] + order_array(order),
-    )
+        [1, market] + order_array(order),
+    ).hex()
+
+
+def operator_hash(market, order, deadline):
+    return web3.solidityKeccak(['bytes32', 'uint64'], [order_hash(market, order), deadline]).hex()
 
 
 def order_array(order):
@@ -19,7 +23,7 @@ def order_array(order):
         order['id'],
         order['isBuy'],
         order['cost'],
-        order['denom'],
+        order['unit'],
         order['expr'],
         order['salt'],
     ]
@@ -32,6 +36,7 @@ def sign(acc, hex):
 
 def test_basic_maker_sell():
     mkt = MarketPlace.deploy({'from': a[0]})
+    mkt.grantRole(mkt.OPERATOR_ROLE(), a[1], {'from': a[0]})
     nft = MockNFT.deploy({'from': a[0]})
     usd = MockERC20.deploy({'from': a[0]})
     order = {
@@ -40,7 +45,7 @@ def test_basic_maker_sell():
         'id': 42,
         'isBuy': False,
         'cost': 100 * 10**18,
-        'denom': usd.address,
+        'unit': usd.address,
         'expr': 2000000000,
         'salt': 0,
     }
@@ -50,8 +55,9 @@ def test_basic_maker_sell():
     usd.approve(mkt, 2**256 - 1, {'from': a[8]})
     assert nft.ownerOf(42) == a[7]
     assert usd.balanceOf(a[8]) == 1000 * 10**18
-    sig = sign(a[7], order_hash(mkt.address, order).hex())
-    mkt.trade(order_array(order), sig, {'from': a[8]})
+    msig = sign(a[7], order_hash(mkt.address, order))
+    osig = sign(a[1], operator_hash(mkt.address, order, 3000000000))
+    mkt.trade(order_array(order), msig, 3000000000, osig, {'from': a[8]})
     assert nft.ownerOf(42) == a[8]
     assert usd.balanceOf(a[7]) == 100 * 10**18
     assert usd.balanceOf(a[8]) == 900 * 10**18
@@ -59,6 +65,7 @@ def test_basic_maker_sell():
 
 def test_basic_maker_buy():
     mkt = MarketPlace.deploy({'from': a[0]})
+    mkt.grantRole(mkt.OPERATOR_ROLE(), a[1], {'from': a[0]})
     nft = MockNFT.deploy({'from': a[0]})
     usd = MockERC20.deploy({'from': a[0]})
     order = {
@@ -67,7 +74,7 @@ def test_basic_maker_buy():
         'id': 42,
         'isBuy': True,
         'cost': 100 * 10**18,
-        'denom': usd.address,
+        'unit': usd.address,
         'expr': 2000000000,
         'salt': 0,
     }
@@ -77,8 +84,10 @@ def test_basic_maker_buy():
     usd.approve(mkt, 2**256 - 1, {'from': a[8]})
     assert nft.ownerOf(42) == a[7]
     assert usd.balanceOf(a[8]) == 1000 * 10**18
-    sig = sign(a[8], order_hash(mkt.address, order).hex())
-    mkt.trade(order_array(order), sig, {'from': a[7]})
+    msig = sign(a[8], order_hash(mkt.address, order))
+    osig = sign(a[1], operator_hash(mkt.address, order, 3000000000))
+    tx = mkt.trade(order_array(order), msig, 3000000000, osig, {'from': a[7]})
+    print(tx.gas_used)
     assert nft.ownerOf(42) == a[8]
     assert usd.balanceOf(a[7]) == 100 * 10**18
     assert usd.balanceOf(a[8]) == 900 * 10**18
